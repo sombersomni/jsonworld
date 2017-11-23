efee16dd7c046bed3e1f569eada40e4fc94dc81c
somberSomni <kartunehustla@gmail.com>
2017-11-23 02:02:43 -0800

    audio working. css started. need to start setting up

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,21 @@
 
 var config = {
-	tracked_songs: [ "tracked_songs/tundra_drums.mp3", "tracked_songs/tundra_synth.mp3" ]
+	tracked_songs: [ "tracked_songs/tundra_drums.mp3", "tracked_songs/tundra_synth.mp3" ] //name your paths uniquely for a better user experience
 };
 
 function seperateSoundName ( path ) {
 
-	console.log( path );
+	var pattern = /\w+(?!\/){1}(?=\.mp3|\.wav|\.ogg){1}/;
+	var matchedString = path.match(pattern);
+	var remappedArr = matchedString[0].split( "_" ).map( function ( each ) {
+		return each.toLowerCase();
+	} );
+	var newString = remappedArr.join( " " );
+	return newString;
 }
 
 function getAudioContext ( length ) {
+
 	var arr = [];
 	for ( var x = 0; x <= length; x++ ) {
 		arr.push( new AudioContext() );
@@ -17,20 +24,23 @@ function getAudioContext ( length ) {
 }
 
 function audioFetcher ( arr, n, pack, ctx, resolve, reject ) {
+
 	var package = pack;
 	var start = n;
 	if( typeof arr === "object" && arr instanceof Array && ( arr.length - 1 ) >= n ){
 		fetch( arr[n] ).then(function(response) {
-			console.log(response);
 			if( response.ok ){
+
 				return response.arrayBuffer();
 			} else {
+
 				reject( { status: response.status, message: response.statusText } );
 			}
 		}).then(function(buffer) {
-			console.log( "downloading tundra drums" );
+			var soundName = seperateSoundName( arr[n] );
+			console.log( "downloading " + soundName );
 			ctx[n].decodeAudioData(buffer, function(decodedData) {
-			    console.log( "tundra drums " + n + "done" );
+			    console.log( soundName + " complete" );
 			    var source = ctx[n].createBufferSource();
 			    source.buffer = decodedData;
 			    var gain = ctx[n].createGain();
@@ -69,7 +79,7 @@ generateContext.then( function ( audioCtx ) {
 	console.log( arrControlArr );
 	/* control each seperate sound here 
 		everything is perfectly in sync
-		
+
 		arrControlArr[0].start();
 		arrControlArr[1].start();
 	*/
